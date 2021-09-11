cd2a512e635f707b05e839793b576c803dbc6471
Aaravwhitehatjr <82259932+Aaravwhitehatjr@users.noreply.github.com>
2021-09-11 11:23:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+color = "red"; 
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 1;
+ctx.rect(150, 143, 430 , 200);
+ctx.stroke();
+
+
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle="blue";
+ctx.arc(300 , 210 ,40 ,0 ,2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.arc( 320,210 ,40 ,0 ,2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.arc( 350,210 ,40 ,0 ,2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle="yellow";
+ctx.arc( 380, , 40 ,0 ,2 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle="green";
+ctx.arc( 400, 200,40 ,0 ,2 * Math.PI);
+ctx.stroke();
+
+function circle(mouse_x , mouse_y)
+{
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(mouse_x, mouse_y, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+}
+
+
+
